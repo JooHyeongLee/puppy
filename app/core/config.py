4d3adfb52fc95c627a55886f178f8f12452db66a
dean <godparty@naver.com>
2023-08-15 00:01:39 +0900

    init project

---
+++ app/core/config.py
@@ -0,0 +1,2 @@
+READ_ONLY_DATABASE_URL = "mysql+mysqlconnector://user:password@127.0.0.1:3307/db"
+WRITE_DATABASE_URL = "mysql+mysqlconnector://user:password@127.0.0.1:3306/db"
---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from app.api.routes import router as api_router
+from app.core.database import get_write_session
+from app.core.config import READ_ONLY_DATABASE_URL
+
+app = FastAPI()
+
+app.include_router(api_router, prefix="/api")
+
+# 데이터베이스 연결 초기화 및 종료 설정
+@app.on_event("startup")
+async def startup_db():
+    # 데이터베이스 연결 초기화
+    get_write_session().configure(bind=READ_ONLY_DATABASE_URL)
+
+@app.on_event("shutdown")
+async def shutdown_db():
+    # 데이터베이스 연결 종료
+    get_write_session().remove()
